ab247f2f84f67f50e561374808e5a477d0fad8e9
Arpit Kumar <64075616+jainarpit547@users.noreply.github.com>
2020-07-29 12:05:50 +0530

    Add files via upload

---
+++ vegan.js
@@ -0,0 +1,14 @@
+var togglIcon=document.getElementById('toggle-icon');
+var sideBar= document.getElementById('side-bar');
+
+togglIcon.onclick=function(){
+  sideBar.style.visibility= "visible";
+}
+
+var demo1=document.getElementById('demo1');
+var menu=document.getElementById('demo-menu-side');
+demo1.onclick= function(){
+  menu.style.visibility= "visible";
+}
+
+
